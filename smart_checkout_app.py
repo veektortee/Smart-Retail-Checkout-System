36431d5dda70a778056d11b2990d52dd8d620466
Victor Taiwo <89054538+veektortee@users.noreply.github.com>
2025-06-13 15:31:27 +0100

    Update smart_checkout_app.py

--- smart_checkout_app.py
+++ smart_checkout_app.py
@@ -10,20 +10,15 @@ from utils import get_price_map, get_class_names
 # ---------------------
 # CONFIGURATION
 # ---------------------
-MODEL_URL = "https://storage.googleapis.com/object-detection-retail/models/best.pt"
 LOCAL_PATH = "best.pt"
 CLASS_NAMES = get_class_names()
 PRICE_MAP = get_price_map()
-EXAMPLE_IMAGE_PATH = "1082ae68-33Booooox_jpg.rf.b1324bf46bbd1400bf00c476877b8b4f.jpg"  # Optional: path to a sample image
+EXAMPLE_IMAGE_PATH = "1082ae68-33Booooox_jpg.rf.b1324bf46bbd1400bf00c476877b8b4f.jpg"  # Optional: path to a sample image. Can adjust to yours.
 
 # ---------------------
 # LOAD MODEL
 # ---------------------
-# Download if not already present
-if not os.path.exists(LOCAL_PATH):
-    with open(LOCAL_PATH, "wb") as f:
-        f.write(requests.get(MODEL_URL).content)
-model = YOLO(LOCAL_PATH, task='detect')
+model = YOLO(LOCAL_PATH)
 
 # ---------------------
 # STREAMLIT UI
