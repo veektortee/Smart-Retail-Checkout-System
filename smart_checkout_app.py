c217fa5e49f6f46efe60229dfcca5023b36c8420
Victor Taiwo <89054538+veektortee@users.noreply.github.com>
2025-06-13 19:54:35 +0100

    download fine-tuned model from storage
    After training, I saved my model on GCS (Google Cloud Storage) buckets for safekeeping. To use it on my deployed Streamlit app, I need to download it from GCS.

--- smart_checkout_app.py
+++ smart_checkout_app.py
@@ -10,7 +10,8 @@ from utils import get_price_map, get_class_names
 # ---------------------
 # CONFIGURATION
 # ---------------------
-LOCAL_PATH = "best.pt"
+MODEL_URL = "https://storage.googleapis.com/object-detection-retail/models/best.torchscript"
+LOCAL_PATH = "best.torchscript"
 CLASS_NAMES = get_class_names()
 PRICE_MAP = get_price_map()
 EXAMPLE_IMAGE_PATH = "1082ae68-33Booooox_jpg.rf.b1324bf46bbd1400bf00c476877b8b4f.jpg"  # Optional: path to a sample image. Can adjust to yours.
@@ -18,6 +19,10 @@ EXAMPLE_IMAGE_PATH = "1082ae68-33Booooox_jpg.rf.b1324bf46bbd1400bf00c476877b8b4f
 # ---------------------
 # LOAD MODEL
 # ---------------------
+# Download if not already present
+if not os.path.exists(LOCAL_PATH):
+    with open(LOCAL_PATH, "wb") as f:
+        f.write(requests.get(MODEL_URl).content)
 model = YOLO(LOCAL_PATH)
 
 # ---------------------
